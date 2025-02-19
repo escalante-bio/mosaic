7181071a96e542473a1e5dd9be0f381b446eb2cf
samtomguns <sam@escalante.bio>
2025-02-19 20:42:49 +0000

    license info

--- src/boltz_binder_design/losses/esm.py
+++ src/boltz_binder_design/losses/esm.py
@@ -1,3 +1,16 @@
+# This file incorporates portions of code from the esm2quinox library,
+# created by Patrick Kidger and licensed under the Apache License, 
+# Version 2.0 (the "License"); you may not use this file except in compliance 
+# with the License. You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import jax
 import numpy as np
 import equinox as eqx
@@ -18,6 +31,7 @@ def boltz_to_esm_matrix():
     return T
 
 def apply_trunk(esm, x, is_pad):
+    """Trunk portion of the forward pass of esm2quinox._esm2.ESM2"""
     dynamic_layers, static_layer = eqx.partition(esm.layers, eqx.is_array)
 
     def f(x, dynamic_layer):
