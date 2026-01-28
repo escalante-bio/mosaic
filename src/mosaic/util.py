3ab64cd134715ae3bd00328d4db5924c3227d38a
samtomguns <sam@escalante.bio>
2026-01-28 19:25:05 +0000

    variable leading dims

--- src/mosaic/util.py
+++ src/mosaic/util.py
@@ -18,9 +18,9 @@ ref_charge[('GLU', 'OE2')] = -1
 ref_charge[('ASP', 'OD2')] = -1
 
 def pairwise_distance(
-    a: Float[Array, "T N D"], b: Float[Array, "T M D"]
-) -> Float[Array, "T N M"]:
-    r = a[:, :, None, :] - b[:, None, :, :]
+    a: Float[Array, "... N D"], b: Float[Array, "... M D"]
+) -> Float[Array, "... N M"]:
+    r = a[..., :, None, :] - b[..., None, :, :]
     return jnp.sqrt(jnp.sum(r * r, axis=-1) + 1e-8)
 
 def get_3d_rot_trans(P, Q):
