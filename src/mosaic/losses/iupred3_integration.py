279a17884921e6910c08d625b93438ad43e1b38a
jpigles <jpiglesias2003@gmail.com>
2026-03-19 13:57:08 -0700

    Add integration functions create_iupred3_loss and sequence_to_loss

---
+++ src/mosaic/losses/iupred3_integration.py
@@ -0,0 +1,7 @@
+def create_iupred3_loss():
+    # Integration function for IUPred3 loss
+    pass
+
+def sequence_to_loss(sequence):
+    # Function to convert a sequence to a loss value
+    return None
